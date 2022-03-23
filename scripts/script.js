52806099e20c00ac2237e0f4496b63ce418af9c5
Kent-Danielle <kconcengco@my.bcit.ca>
2022-03-22 23:57:29 -0700

    made login.html restrict users from accessing FindaPotty without logging in

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,50 @@
+// function to get a global variable for the user
+var currentUser;
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user);
+    currentUser = db.collection("Users").doc(user.uid); //global
+    console.log(currentUser);
+
+    // the following functions are always called when someone is logged in
+  } else {
+    // No user is signed in.
+    console.log("No user is signed in");
+    window.location.href = "login.html";
+  }
+});
+
+
+
 function sayHello() {
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      // Do something for the user here.
-      console.log(user.uid);
-      db.collection("users")
-        .doc(user.uid)
-        .get()
-        .then(function (doc) {
-          var n = doc.data().name;
-          console.log(n);
-          //$("#username").text(n);
-          document.getElementById("username").innerText = n;
-        });
-    } else {
-      // No user is signed in.
-    }
-  });
+  // User is signed in.
+  // Do something for the user here.
+  console.log(currentUser.id);
+  db.collection("Users")
+    .doc(currentUser.id)
+    .get()
+    .then(function (doc) {
+      var n = doc.data().name;
+      console.log(n);
+    });
+  if (currentUser) {
+    console.log("Logged in");
+  } else {
+    console.log("not logged in");
+  }
 }
-//sayHello();
+
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  console.log("logging out user");
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      window.location.href = "index.html";
+    }).catch((error) => {
+      // An error happened.
+    });
+}
+
