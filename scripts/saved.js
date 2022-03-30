788d206dd1e4064633c49ed0e05d9b43ee41ef6f
Kent-Danielle <kconcengco@my.bcit.ca>
2022-03-29 22:02:58 -0700

    updated profile.html and profile.css to show bookmarks

--- scripts/saved.js
+++ scripts/saved.js
@@ -17,8 +17,13 @@ function displayPotties(collection) {
           //title
           newcard.querySelector("#pottyTitle").innerHTML = title;
 
+          //update image
+          if (doc.data().potty_pic != null) {
+            newcard.querySelector("#pottyImage").src = doc.data().potty_pic;
+          }
+
           //check for ratings
-          for (i = 0; i < ratings; i++) {
+          for (n = 0; n < ratings; n++) {
             starRatings =
               starRatings + "<i class='text-warning material-icons'>star</i>";
           }
@@ -26,33 +31,27 @@ function displayPotties(collection) {
 
           //check for features
           if (doc.data().elderly_accessible == true) {
-            features += "<i class='text-success material-icons'>elderly</i>";
+            features += "<i class='text-accent material-icons'>elderly</i>";
           }
           if (doc.data().wheelchair_accessible == true) {
-            features += "<i class='text-success material-icons'>accessible</i>";
+            features += "<i class='text-accent material-icons'>accessible</i>";
           }
           if (doc.data().diaper_station == true) {
             features +=
-              "<i class='text-success material-icons'>baby_changing_station</i>";
+              "<i class='text-accent material-icons'>baby_changing_station</i>";
           }
           if (doc.data().pregnant_accessible == true) {
             features +=
-              "<i class='text-success material-icons'>pregnant_woman</i>";
+              "<i class='text-accent material-icons'>pregnant_woman</i>";
           }
           if (doc.data().LGBT_accessible == true) {
-            features +=
-              "<i class='text-success material-icons'>transgender</i>";
-          }
-          newcard.querySelector(".features").innerHTML = features;
-
-          //change heart icon according to saved state
-          if (doc.data().saved == true) {
-            newcard.querySelector(".favorite").innerHTML = "favorite";
-          } else {
-            newcard.querySelector(".favorite").innerHTML = "favorite_border";
+            features += "<i class='text-accent material-icons'>transgender</i>";
           }
 
-          document.getElementById(collection + "-go-here").appendChild(newcard);
+          newcard.querySelector(".link-spanner").onclick = () =>
+            setPottyData(doc.id);
+
+          document.getElementById("Bookmarks-go-here").appendChild(newcard);
           i++;
         } else {
           i++;
@@ -62,3 +61,7 @@ function displayPotties(collection) {
 }
 
 displayPotties("Potties");
+
+function setPottyData(id) {
+  localStorage.setItem("pottyID", id);
+}
